8d814d58348d9f5f757025f53dfa32295b08771f
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-11-15 16:24:23 -0500

    Added hypercalls. No printing, but no errors as well

--- src/crust.rs
+++ src/crust.rs
@@ -1,11 +1,15 @@
 #![feature(no_std)]
 #![feature(lang_items)]
+#![feature(asm)]
+#![feature(core_str_ext)]
 
 #![no_std]
 
 pub mod shims;
+pub mod hypercalls;
 
 #[no_mangle]
 pub extern fn main() {
+    hypercalls::say_hello();
     while true {}
 }
---
+++ src/hypercalls.rs
@@ -0,0 +1,40 @@
+macro_rules! hypercall3 {
+    ( $name : expr, $a1 : expr, $a2 : expr, $a3 : expr ) => {
+        unsafe { 
+            let mut __res : u64 = 0;
+            let mut __ign1 : u64 = 0;
+            let mut __ign2 : u64 = 0;
+            let mut __ign3 : u64 = 0;
+            let a1 : i64 = $a1 as i64;
+            let a2 : i64 = $a2 as i64;
+            let a3 : i64 = $a3 as i64;
+            //: "=a"(__res), "=edi"(__ign1), "=esi"(__ign2),"=d"(__ign3)
+            asm!(concat!("call HYPERCALL_PAGE + ",$name,"*32")
+                : "={ax}"(__res),"={Di}"(__ign1),"={Si}"(__ign2),"={dx}"(__ign3)
+                : "1"(a1), "2"(a2), "3"(a3)				
+                : "memory" 
+                : "volatile"
+                );					
+        }
+    };
+
+}
+
+macro_rules! CONSOLEIO {
+    () => { 18 };
+}
+
+const CONSOLEIO_WRITE : usize = 0;
+const CONSOELIO_READ : usize = 1;
+
+#[no_mangle]
+pub extern fn print(s : *const u8, len : usize) {
+    hypercall3!(CONSOLEIO!(), CONSOLEIO_WRITE, len, s);
+}
+
+
+#[no_mangle]
+pub extern fn say_hello(){
+    let hell = "hell FROM RUST";
+    print(hell.as_ptr(), hell.len());
+}
