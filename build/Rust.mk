d193dec8bc05e6001cf49e75e30bac2a592f10ae
Carl Patenaude Poulin <carl.patenaudepoulin@mail.mcgill.ca>
2015-12-25 15:08:25 -0500

    fixed missing libcore

--- build/Rust.mk
+++ build/Rust.mk
@@ -14,7 +14,8 @@ RUST_FILES = $(shell find $(SRC) -name "*.rs")
 $(DEPS)/$(OBJ)/libcrust.a.d: $(RUST_FILES)
 -include $(DEPS)/$(OBJ)/libcrust.a.d
 
-$(OBJ)/libcrust.a: $(CARGO_DEPS) $(LIB)/libcore.rlib
+$(OBJ)/libcrust.a: $(CARGO_DEPS)
 	$(MKDIR) $(@D)
+	$(warning if the following fails with "error: can't find crate for `core`", you need to `make lib/libcore.rlib` and put it in your rust toolchain directory under lib/rustlib/$(TARGET)/lib.)
 	$(CARGO) build $(CARGO_ARGS)
 
