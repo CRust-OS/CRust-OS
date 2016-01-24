60cf2d41cc9eee0906956d7cf4bf85ad2d4bf287
Jit Kanetkar <satyajit.kanetkar@mail.mcgill.ca>
2016-01-24 17:17:42 -0500

    added sbrk, switched from *mut u8 to usize, and added defines to make

--- build/malloc.mk
+++ build/malloc.mk
@@ -4,11 +4,11 @@ MALLOC_SRC = mm/src/malloc.c
 MALLOC_OUT = $(MALLOC_TMP)/malloc.o
 
 CFLAGS := -nodefaultlibs -c -fno-stack-protector
-DEFINE := -DHAVE_MMAP=0 -DHAVE_MREMAP=
+DEFINE := -DHAVE_MMAP=0 -DHAVE_MREMAP=0
 
 $(MALLOC_OUT): $(MALLOC_SRC)
 	$(MKDIR) $(@D)
-	$(cc) $(CFLAGS) $(MALLOC_SRC) -o $(MALLOC_OUT)
+	$(cc) $(DEFINE) $(CFLAGS) $(MALLOC_SRC) -o $(MALLOC_OUT)
 
 $(LIB)/malloc: $(MALLOC_OUT)
 	$(ECHO) "TODO: make malloc into a proper object file"
--- mm/src/mm.rs
+++ mm/src/mm.rs
@@ -6,17 +6,39 @@
 #![allocator]
 
 
-
-static mut heap_end : *mut u8 = 0 as *mut u8;
+static mut heap_end : usize = 0;
+static mut __errno_location : usize = 0;
 
 extern {
     static HEAP : *const u8;
 }
 
-///! Set `heap_end` to the starting location of the heap
-pub fn setup() {
+#[no_mangle]
+pub extern fn setup() {
+    sbrk(0);
+}
+
+///! Increase the end of the heap. Also initializes
+///! heap on first call. Based off [newlib implementation]
+///! (https://sourceware.org/newlib/libc.html#Syscalls)
+#[no_mangle]
+pub extern fn sbrk(incr: usize) -> *const u8 {
     unsafe {
-        *heap_end = *HEAP;
+        let previous_heap_end : usize;
+        if heap_end == 0 {
+                heap_end = HEAP as usize;
+        }
+
+        let previous_heap_end = heap_end;
+
+        // detect unsiged overflow
+        // TODO apparently Rust detects unsigned integer overflow...
+        if (heap_end + incr) < heap_end {
+            //TODO fail gracefully
+            loop {};
+        }
+        heap_end += incr;
+        previous_heap_end as *const u8
     }
 }
 
@@ -25,8 +47,8 @@ pub fn setup() {
 ///! Currently, the pointer can't be freed
 pub extern fn __rust_allocate(size: usize, _align: usize) -> *mut u8 {
     unsafe {
-        heap_end = ((heap_end as usize) + size + (_align - size % _align)) as *mut u8;
-        heap_end
+        heap_end = heap_end + size + (_align - size % _align);
+        heap_end as *mut u8
     }
 }
 
