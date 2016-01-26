865d70d36168237d32e62062505592fb6410ed1e
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2016-01-26 10:24:34 -0500

    Implemented ring buffer trait, still some work to do though

--- src/xen/ring_buffer/mod.rs
+++ src/xen/ring_buffer/mod.rs
@@ -1,3 +1,6 @@
+use ::xen::arch::mem::*;
+use ::xen::event_channels::send;
+
 // TODO: Break up Ring Trait in WriteableRing and ReadableRing
 pub trait Ring {
     fn input_buffer(&mut self) -> &mut [u8];
@@ -23,8 +26,39 @@ pub trait Ring {
         return len - 1;
     }
 
-    fn write(&mut self) {
+    fn write(&mut self, buf : &[u8]) {
+        let mut sent = 0usize;
+
+        let cons = self.get_out_cons(); 
+        let mut prod = self.get_out_prod();
+
+        mb();
+
+        {
+            let output_mask = { self.output_mask() };
+            let output_len = { self.output_buffer().len() };
+            let mut output = self.output_buffer();
+
+            while (sent < buf.len()) && ((prod - cons) < output_len) {
+                let idx = prod & output_mask; // mask the index to make sure we don't overflow
+                prod = prod + 1;
+                output[idx] = buf[sent];
+                sent = sent + 1;
+            }
+        }
+
+        wmb();
+
+        self.set_out_prod(prod);
+
     }
+
+    fn write_notify(&mut self, buf : &[u8], evtchn : u32) {
+        self.write(buf);
+        unsafe { send(evtchn); }
+    }
+
     fn read(&mut self) {
+        // TODO
     }
 }
